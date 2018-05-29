5f5101ed0f530e2aeb4545de3547991d213726ee
Lauren Jones <ljones@euclideon.com>
2018-05-29 16:54:13 +1000

    Added MGA Support (Elementary definition of GDA94)

--- src/vcGIS.cpp
+++ src/vcGIS.cpp
@@ -37,6 +37,17 @@ bool vcGIS_PopulateEPSGParameters(vcGIS_EPSGParameters *pParams, uint16_t epsgCo
     pParams->a = 6378137;
     pParams->k = 0.9996;
   }
+  else if (epsgCode > 28347 && epsgCode < 28357)
+  {
+    // GDA94 Southern Hemisphere (for MGA)
+    pParams->zone = epsgCode - 28300;
+    pParams->meridian = pParams->zone * 6 - 183;
+    pParams->falseNorthing = 10000000;
+    pParams->falseEasting = 500000;
+    pParams->f = 1 / 298.257222101;
+    pParams->a = 6378137;
+    pParams->k = 0.9996;
+  }
   else
   {
     return false;
