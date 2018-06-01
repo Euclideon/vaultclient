2a4306e4dd0604d3a4bd5703217a809cb71aa99e
Paul Fox <pfox@euclideon.com>
2018-06-01 13:11:46 +1000

    Added a proper settings saving (with defaults)
    - Window correctly remembers size and location when closed (Resolves EVA-45)
    - Frames correctly return to how they were when closed (Resolves EVA-81)
    - Added System->Restore Defaults option to reset everything (Resolves EVA-144)

--- src/imgui_ex/imgui_dock.h
+++ src/imgui_ex/imgui_dock.h
@@ -7,7 +7,4 @@ namespace ImGui
   IMGUI_API bool BeginDock(const char* label, bool* opened = nullptr, ImGuiWindowFlags extra_flags = 0);
   IMGUI_API void EndDock();
   IMGUI_API void SetDockActive();
-  IMGUI_API void LoadDock();
-  IMGUI_API void SaveDock();
-  IMGUI_API void Print();
 } // namespace ImGui
