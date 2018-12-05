cd5459eaabeb149543b7a42222377a7434bd19b3
Paul Fox <pfox@euclideon.com>
2018-12-05 17:33:54 +1000

    Added support for position & rotation overrides from UDP (building block for projects)

--- src/legacy/vcUDP.cpp
+++ src/legacy/vcUDP.cpp
@@ -3,7 +3,7 @@
 #include "udPlatform/udFile.h"
 #include "udPlatform/udJSON.h"
 
-void vcUDP_AddModel(vcState *pProgramState, const char *pUDPFilename, const char *pModelFilename, bool firstLoad)
+void vcUDP_AddModel(vcState *pProgramState, const char *pUDPFilename, const char *pModelFilename, bool firstLoad, udDouble3 *pPosition, udDouble3 *pYPR, double scale)
 {
   // If the model filename is nullptr there's nothing to load
   if (pModelFilename == nullptr)
@@ -20,7 +20,7 @@ void vcUDP_AddModel(vcState *pProgramState, const char *pUDPFilename, const char
   else
     file.SetFromFullPath(pModelFilename);
 
-  vcModel_AddToList(pProgramState, file, firstLoad);
+  vcModel_AddToList(pProgramState, file, firstLoad, pPosition, pYPR, scale);
 }
 
 void vcUDP_Load(vcState *pProgramState, const char *pFilename)
@@ -40,7 +40,7 @@ void vcUDP_Load(vcState *pProgramState, const char *pFilename)
       const char *pName = dataEntries.Get("[%zu].Name", i).AsString();
       if (udStrEqual(pName, "AbsoluteModelPath"))
       {
-        vcUDP_AddModel(pProgramState, pFilename, dataEntries.Get("[%zu].content", i).AsString(), firstLoad);
+        vcUDP_AddModel(pProgramState, pFilename, dataEntries.Get("[%zu].content", i).AsString(), firstLoad, nullptr, nullptr, 1.0);
         firstLoad = false;
       }
     }
@@ -56,13 +56,68 @@ void vcUDP_Load(vcState *pProgramState, const char *pFilename)
           if (udStrEqual(datasets.Get("[%zu].Name", j).AsString(), "DataSetData"))
           {
             const udJSON &datasetData = datasets.Get("[%zu].DataEntry", j);
+
+            const char *pName = nullptr;
+            const char *pPath = nullptr;
+            const char *pLocation = nullptr;
+            const char *pAngleX = nullptr;
+            const char *pAngleY = nullptr;
+            const char *pAngleZ = nullptr;
+
             for (size_t k = 0; k < datasetData.ArrayLength(); k++)
             {
-              if (udStrEqual(datasetData.Get("[%zu].Name", k).AsString(), "Path"))
+              if (udStrEqual(datasetData.Get("[%zu].Name", k).AsString(), "Name"))
+                pName = datasetData.Get("[%zu].content", k).AsString();
+              else if (udStrEqual(datasetData.Get("[%zu].Name", k).AsString(), "Path"))
+                pPath = datasetData.Get("[%zu].content", k).AsString();
+              else if (udStrEqual(datasetData.Get("[%zu].Name", k).AsString(), "Location"))
+                pLocation = datasetData.Get("[%zu].content", k).AsString();
+              else if (udStrEqual(datasetData.Get("[%zu].Name", k).AsString(), "AngleX"))
+                pAngleX = datasetData.Get("[%zu].content", k).AsString();
+              else if (udStrEqual(datasetData.Get("[%zu].Name", k).AsString(), "AngleY"))
+                pAngleY = datasetData.Get("[%zu].content", k).AsString();
+              else if (udStrEqual(datasetData.Get("[%zu].Name", k).AsString(), "AngleZ"))
+                pAngleZ = datasetData.Get("[%zu].content", k).AsString();
+            }
+
+            if (pPath != nullptr)
+            {
+              udDouble3 position = udDouble3::zero();
+              udDouble3 ypr = udDouble3::zero();
+              double scale = 1.0;
+
+              udDouble3 *pPosition = nullptr;
+              udDouble3 *pYPR = nullptr;
+
+              if (pLocation != nullptr)
               {
-                vcUDP_AddModel(pProgramState, pFilename, datasetData.Get("[%zu].content", k).AsString(), firstLoad);
-                firstLoad = false;
+                //TODO: Handle the SRID that lives here as well for some reason
+
+                int offset = 0;
+                int offset2 = 0;
+                position.x = udStrAtof64(pLocation, &offset);
+                position.y = udStrAtof64(&pLocation[offset + 1], &offset2);
+                position.z = udStrAtof64(&pLocation[offset + offset2 + 2]);
+
+                pPosition = &position;
               }
+
+              if (pAngleX != nullptr || pAngleY != nullptr || pAngleZ != nullptr) // At least one of the angles is set
+              {
+                if (pAngleZ != nullptr)
+                  ypr.x = UD_DEG2RAD(udStrAtof64(pAngleZ));
+
+                if (pAngleX != nullptr)
+                  ypr.y = UD_DEG2RAD(udStrAtof64(pAngleX));
+
+                if (pAngleY != nullptr)
+                  ypr.z = UD_DEG2RAD(udStrAtof64(pAngleY));
+
+                pYPR = &ypr;
+              }
+
+              vcUDP_AddModel(pProgramState, pFilename, pPath, firstLoad, pPosition, pYPR, scale);
+              firstLoad = false;
             }
           }
         }
