569e2a0639cc8f5999715bd8a8f258a28be06cdd
Paul Fox <pfox@euclideon.com>
2018-12-10 14:02:14 +1000

    Changed the threading with vcModals_DrawTileServer to not have race conditions

--- src/vcState.h
+++ src/vcState.h
@@ -84,16 +84,18 @@ struct vcState
   udJSON projects;
   udJSON packageInfo;
 
-  vcTexture *pTileServerIcon;
-
   struct
   {
     vcGizmoOperation operation;
     vcGizmoCoordinateSystem coordinateSystem;
   } gizmo;
 
-  volatile void *pImageData = nullptr;
-  volatile int64_t imageSize = -1;
+  struct
+  {
+    vcTexture *pServerIcon;
+    volatile void *pImageData;
+    volatile int64_t loadStatus; // >0 is the size of pImageData
+  } tileModal;
 };
 
 #endif // !vcState_h__
