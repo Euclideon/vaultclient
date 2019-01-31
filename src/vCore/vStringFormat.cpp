e92fa3ba8ecf0e208a0b22f74c6d4984c2c4290a
Paul Fox <pfox@euclideon.com>
2019-02-01 08:47:54 +1000

    Updated the language file and shifted lots of the work to the runtime for better localization support

--- src/vCore/vStringFormat.cpp
+++ src/vCore/vStringFormat.cpp
@@ -35,6 +35,7 @@ const char *vStringFormat(const char *pFormatString, const char **ppStrings, siz
       i += length - 1;
     }
   }
+
   int newLength = len + newChars + 1;
   pBuf = udAllocType(char, newLength, udAF_None);
 
@@ -86,7 +87,99 @@ const char *vStringFormat(const char *pFormatString, const char **ppStrings, siz
   return pBuf;
 }
 
+const char *vStringFormat(char *pBuffer, size_t bufLen, const char *pFormatString, const char **ppStrings, size_t numStrings)
+{
+  int len;
+  for (len = 0; pFormatString[len] != '\0'; ++len)
+    if (pFormatString[len] == '|')
+      ++len;
+  int newChars = 0;
+  int currArg;
+
+  for (int i = 0; pFormatString[i] != '\0'; ++i)
+  {
+    if (pFormatString[i] == '|')
+    {
+      ++i;
+      --newChars;
+      continue;
+    }
+    // Format specifier
+    if (pFormatString[i] == '{')
+    {
+      if (len <= i + 1 || pFormatString[i + 1] < '0' || pFormatString[i + 1] > '9')
+        continue;
+
+      int length;
+      currArg = udStrAtoi((pFormatString + i + 1), &length);
+      if (currArg > (int)numStrings || currArg < 0 || pFormatString[length + i + 1] != '}')
+        continue;
+
+      newChars += (int)udStrlen(ppStrings[currArg]);
+      newChars -= length + 2;
+      i += length - 1;
+    }
+  }
+
+  int newLength = len + newChars + 1;
+
+  if (bufLen < newLength)
+    return pFormatString;
+
+  int outPos = 0;
+  for (int i = 0; pFormatString[i] != '\0'; ++i)
+  {
+    if (pFormatString[i] == '|')
+    {
+      ++i;
+      pBuffer[outPos] = pFormatString[i];
+      ++outPos;
+      continue;
+    }
+    // Format Specifier
+    if (pFormatString[i] == '{')
+    {
+      if (len <= i + 1 || pFormatString[i + 1] < '0' || pFormatString[i + 1] > '9')
+      {
+        pBuffer[outPos] = pFormatString[i];
+        ++outPos;
+        continue;
+      }
+
+      int length;
+      currArg = udStrAtoi((pFormatString + i + 1), &length);
+      if (currArg > (int)numStrings || currArg < 0 || pFormatString[length + i + 1] != '}')
+      {
+        pBuffer[outPos] = pFormatString[i];
+        ++outPos;
+        continue;
+      }
+
+      int k;
+      for (k = 0; ppStrings[currArg][k] != '\0'; ++k)
+        pBuffer[outPos + k] = ppStrings[currArg][k];
+
+      i += length + 1;
+      outPos += k - 1;
+    }
+    else
+    {
+      pBuffer[outPos] = pFormatString[i];
+    }
+
+    ++outPos;
+  }
+  pBuffer[outPos] = '\0';
+
+  return pBuffer;
+}
+
 const char *vStringFormat(const char *pFormatString, const char *pString)
 {
   return vStringFormat(pFormatString, &pString, 1);
+}
+
+const char *vStringFormat(char *pBuffer, size_t bufLen, const char *pFormatString, const char *pString)
+{
+  return vStringFormat(pBuffer, bufLen, pFormatString, &pString, 1);
 }
