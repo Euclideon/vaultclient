34add2d34d18c8c541cd2907891ffcc17ea8f738
Paul Fox <pfox@euclideon.com>
2019-02-01 09:13:37 +1000

    Added missing include for stdint

--- src/vCore/vStringFormat.h
+++ src/vCore/vStringFormat.h
@@ -1,6 +1,8 @@
 #ifndef vStringFormat_H__
 #define vStringFormat_H__
 
+#include "udPlatform/udPlatform.h"
+
 // Returns formatted string
 const char* vStringFormat(const char *pFormatString, const char **ppStrings, size_t numStrings);
 const char* vStringFormat(const char *pFormatString, const char *pStrings); // Helper for the case where there is only 1 string
