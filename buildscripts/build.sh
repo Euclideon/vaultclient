7c702731f8480e8cd708e485d9a33b9e2c1d8764
Samuel Surtees <ssurtees@euclideon.com>
2019-02-11 21:41:58 +1000

    Added ability to remove watermarks on convert tab
    - Fixed issue with temp directory incorrectly changing

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -13,7 +13,7 @@ else
 fi
 
 export DEPLOYDIR="$DEV/Builds/vault/client/Pipeline_$CI_PIPELINE_ID"
-export VAULTSDK_HOME="$DEV/Builds/vault/linkedvdk/Pipeline_34014"
+export VAULTSDK_HOME="$DEV/Builds/vault/linkedvdk/Pipeline_34181"
 
 # Prepare UserGuide
 mkdir -p builds/userguide
