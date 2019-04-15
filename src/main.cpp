41272cc60c0d6558a98eac67b1d0141cf5a213af
Jonathan Kable <jkable@euclideon.com>
2019-04-15 15:03:23 +1000

    Added Add AOI and Add Line functionality to right-click menu and to scene explorer buttons. Also moved Add Node to the top of said menu.

--- src/main.cpp
+++ src/main.cpp
@@ -1108,10 +1108,41 @@ void vcRenderSceneWindow(vcState *pProgramState)
 
       if (hadMouse)
       {
-        if (ImGui::MenuItem(vcString::Get("sceneAddPOI")))
+        if (pProgramState->sceneExplorer.selectedItems.size() == 1)
         {
-          vcScene_AddItem(pProgramState, new vcPOI(vcString::Get("scenePOIDefaultName"), 0xFFFFFFFF, vcLFS_Medium, worldMouse, pProgramState->gis.SRID), true);
-          ImGui::CloseCurrentPopup();
+          const vcSceneItemRef &item = pProgramState->sceneExplorer.selectedItems[0];
+          if (item.pParent->m_children[item.index]->m_type == vcSOT_PointOfInterest)
+          {
+            vcPOI* pPOI = (vcPOI*)item.pParent->m_children[item.index];
+
+            if (ImGui::MenuItem(vcString::Get("scenePOIAddPoint")))
+              pPOI->AddPoint(worldMouse);
+          }
+        }
+
+        if (ImGui::BeginMenu(vcString::Get("sceneAddMenu")))
+        {
+          if (ImGui::MenuItem(vcString::Get("sceneAddPOI")))
+          {
+            vcScene_AddItem(pProgramState, new vcPOI(vcString::Get("scenePOIDefaultName"), 0xFFFFFFFF, vcLFS_Medium, worldMouse, pProgramState->gis.SRID), false);
+            ImGui::CloseCurrentPopup();
+          }
+          if (ImGui::MenuItem(vcString::Get("sceneAddAOI")))
+          {
+            vcScene_ClearSelection(pProgramState);
+            vcPOI *pAOI = new vcPOI(vcString::Get("scenePOIAreaDefaultName"), 0xFFFFFFFF, vcLFS_Medium, worldMouse, pProgramState->gis.SRID);
+            pAOI->m_line.closed = true;
+            vcScene_AddItem(pProgramState, pAOI, true);
+            ImGui::CloseCurrentPopup();
+          }
+          if (ImGui::MenuItem(vcString::Get("sceneAddLine")))
+          {
+            vcScene_ClearSelection(pProgramState);
+            vcScene_AddItem(pProgramState, new vcPOI(vcString::Get("scenePOILineDefaultName"), 0xFFFFFFFF, vcLFS_Medium, worldMouse, pProgramState->gis.SRID), true);
+            ImGui::CloseCurrentPopup();
+          }
+
+          ImGui::EndMenu();
         }
 
         if (pProgramState->settings.maptiles.mapEnabled && pProgramState->gis.isProjected && pProgramState->settings.maptiles.mapHeight != worldMouse.z)
@@ -1130,18 +1161,6 @@ void vcRenderSceneWindow(vcState *pProgramState)
           pProgramState->cameraInput.startAngle = udDoubleQuat::create(pProgramState->pCamera->eulerRotation);
           pProgramState->cameraInput.worldAnchorPoint = worldMouse;
           pProgramState->cameraInput.progress = 0.0;
-        }
-
-        if (pProgramState->sceneExplorer.selectedItems.size() == 1)
-        {
-          const vcSceneItemRef &item = pProgramState->sceneExplorer.selectedItems[0];
-          if (item.pParent->m_children[item.index]->m_type == vcSOT_PointOfInterest)
-          {
-            vcPOI* pPOI = (vcPOI*)item.pParent->m_children[item.index];
-
-            if (ImGui::MenuItem(vcString::Get("scenePOIAddPoint")))
-              pPOI->AddPoint(worldMouse);
-          }
         }
       }
       else
@@ -1224,7 +1243,7 @@ void vcRenderSceneWindow(vcState *pProgramState)
   renderData.labels.Deinit();
   renderData.waterVolumes.Deinit();
   renderData.polyModels.Deinit();
-  renderData.labels.Deinit();
+  renderData.images.Deinit();
 
   pProgramState->previousWorldMousePos = renderData.worldMousePos;
   pProgramState->previousPickingSuccess = renderData.pickingSuccess;
@@ -1666,6 +1685,7 @@ void vcRenderWindow(vcState *pProgramState)
       ImGui::End();
     }
   }
+
   else
   {
     if (ImGui::BeginDock(udTempStr("%s###sceneExplorerDock", vcString::Get("sceneExplorerTitle")), &pProgramState->settings.window.windowsOpen[vcDocks_SceneExplorer]))
@@ -1674,13 +1694,22 @@ void vcRenderWindow(vcState *pProgramState)
         vcModals_OpenModal(pProgramState, vcMT_AddUDS);
 
       if (vcMenuBarButton(pProgramState->pUITexture, vcString::Get("sceneExplorerAddPOI"), nullptr, vcMBBI_AddPointOfInterest, vcMBBG_SameGroup))
-        vcScene_AddItem(pProgramState, new vcPOI(vcString::Get("scenePOIDefaultName"), 0xFFFFFFFF, vcLFS_Medium, pProgramState->pCamera->position, pProgramState->gis.SRID), true);
+        vcScene_AddItem(pProgramState, new vcPOI(vcString::Get("scenePOIDefaultName"), 0xFFFFFFFF, vcLFS_Medium, pProgramState->pCamera->position, pProgramState->gis.SRID), false);
 
       if (vcMenuBarButton(pProgramState->pUITexture, vcString::Get("sceneExplorerAddAOI"), nullptr, vcMBBI_AddAreaOfInterest, vcMBBG_SameGroup))
-        vcModals_OpenModal(pProgramState, vcMT_NotYetImplemented);
+      {
+        vcScene_ClearSelection(pProgramState);
+        vcPOI *pAOI = new vcPOI(vcString::Get("scenePOIAreaDefaultName"), 0xFFFFFFFF, vcLFS_Medium, pProgramState->pCamera->position, pProgramState->gis.SRID);
+        pAOI->m_line.closed = true;
+        vcScene_AddItem(pProgramState, pAOI, true);
+        ImGui::CloseCurrentPopup();
+      }
 
-      if (vcMenuBarButton(pProgramState->pUITexture, vcString::Get("sceneExplorerAddLines"), nullptr, vcMBBI_AddLines, vcMBBG_SameGroup))
-        vcModals_OpenModal(pProgramState, vcMT_NotYetImplemented);
+      if (vcMenuBarButton(pProgramState->pUITexture, vcString::Get("sceneExplorerAddLine"), nullptr, vcMBBI_AddLines, vcMBBG_SameGroup))
+      {
+        vcScene_ClearSelection(pProgramState);
+        vcScene_AddItem(pProgramState, new vcPOI(vcString::Get("scenePOILineDefaultName"), 0xFFFFFFFF, vcLFS_Medium, pProgramState->pCamera->position, pProgramState->gis.SRID), true);
+      }
 
       vcMenuBarButton(pProgramState->pUITexture, vcString::Get("sceneExplorerAddOther"), nullptr, vcMBBI_AddOther, vcMBBG_SameGroup);
       if (ImGui::BeginPopupContextItem(vcString::Get("sceneExplorerAddOther"), 0))
