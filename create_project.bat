2e1b05c72ee7cafbbe5c327856734207a763953e
Samuel Surtees <ssurtees@euclideon.com>
2019-05-13 20:39:18 +1000

    Added ability to generate Emscripten projects with VaultSDK

--- create_project.bat
+++ create_project.bat
@@ -5,9 +5,11 @@ ECHO 1. Visual Studio 2015 Solution
 ECHO 2. Visual Studio 2015 Solution With vaultSDK Repository
 ECHO 3. Visual Studio 2015 Solution (DirectX 11)
 ECHO 4. Visual Studio 2015 Solution With vaultSDK Repository (DirectX 11)
+ECHO 5. Visual Studio 2015 Solution With vaultSDK Repository (Emscripten)
 
-CHOICE /N /C:1234 /M "[1-4]:"
+CHOICE /N /C:12345 /M "[1-5]:"
 
+IF ERRORLEVEL ==5 GOTO FIVE
 IF ERRORLEVEL ==4 GOTO FOUR
 IF ERRORLEVEL ==3 GOTO THREE
 IF ERRORLEVEL ==2 GOTO TWO
@@ -32,6 +34,11 @@ GOTO END
 :FOUR
  ECHO Creating VS2015 Project (With vaultSDK Repository)...
  bin\premake\premake5.exe vs2015 --force-vaultsdk --gfxapi=d3d11
+ GOTO END
+
+:FIVE
+ ECHO Creating VS2015 Project (With vaultSDK Repository) for Emscripten...
+ bin\premake\premake5.exe vs2015 --force-vaultsdk --os=emscripten
  GOTO END
 
 :END
