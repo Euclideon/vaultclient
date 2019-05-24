d640cc2705b93d347b08cfbaae454dc20faf9237
Paul Fox <pfox@euclideon.com>
2019-05-24 16:02:44 +1000

    Removed memory tracking define

--- premake5.lua
+++ premake5.lua
@@ -198,7 +198,6 @@ solution "vaultClient"
 		defines { "BUILD_TIME=" .. os.getenv("UNIXTIME") }
 	end
 
-	defines { "__MEMORY_DEBUG__" }
 	if _OPTIONS["force-vaultsdk"] then
 		projectSuffix = "VDK"
   
