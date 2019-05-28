add04dba67edad50cdccb50fc38ee8127fccb57e
Paul Fox <pfox@euclideon.com>
2019-05-28 10:52:59 +1000

    Updated VDK pipeline for save support

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -13,7 +13,7 @@ else
 fi
 
 export DEPLOYDIR="$DEV/Builds/vault/client/Pipeline_$CI_PIPELINE_ID"
-export VAULTSDK_HOME="$DEV/Builds/vault/linkedvdk/Pipeline_39146"
+export VAULTSDK_HOME="$DEV/Builds/vault/linkedvdk/Pipeline_39166"
 
 # Prepare UserGuide
 mkdir -p builds/userguide
