7776bf054b6d413aea3bb9b45814a44f713245cf
Paul Fox <pfox@euclideon.com>
2019-05-28 16:50:46 +1000

    Added modal for when loading an unsupported project and updated VDK for better handling of invalid projects

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -13,7 +13,7 @@ else
 fi
 
 export DEPLOYDIR="$DEV/Builds/vault/client/Pipeline_$CI_PIPELINE_ID"
-export VAULTSDK_HOME="$DEV/Builds/vault/linkedvdk/Pipeline_39198"
+export VAULTSDK_HOME="$DEV/Builds/vault/linkedvdk/Pipeline_39253"
 
 # Prepare UserGuide
 mkdir -p builds/userguide
