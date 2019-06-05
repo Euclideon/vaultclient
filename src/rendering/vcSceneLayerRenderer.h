8f09795fcd69183cc6a7a0b1a87c341ecec24366
Peter Adams <padams@euclideon.com>
2019-06-05 16:04:20 +1000

    Node loading priority is now loaded based on distance to camera. Turned off mip generation for IS3 nodes. Partial loading of nodes now occurs. Textures can now be unloaded / reloaded based on memory limits
    Resolves EVC-553
    Resolves EVC-548

--- src/rendering/vcSceneLayerRenderer.h
+++ src/rendering/vcSceneLayerRenderer.h
@@ -3,15 +3,20 @@
 
 #include "vcSceneLayer.h"
 
+struct vcTexture;
+
 struct vcSceneLayerRenderer
 {
   vcSceneLayer *pSceneLayer;
   udDouble4 frustumPlanes[6];
+
+  vcTexture *pEmptyTexture;
+  udDouble3 cameraPosition;
 };
 
 udResult vcSceneLayerRenderer_Create(vcSceneLayerRenderer **ppSceneLayerRenderer, udWorkerPool *pWorkerThreadPool, const char *pSceneLayerURL);
 udResult vcSceneLayerRenderer_Destroy(vcSceneLayerRenderer **ppSceneLayerRenderer);
 
-bool vcSceneLayerRenderer_Render(vcSceneLayerRenderer *pSceneLayerRenderer, const udDouble4x4 &worldMatrix, const udDouble4x4 &viewProjectionMatrix, const udUInt2 &screenResolution, const udFloat4 *pColourOverride = nullptr);
+bool vcSceneLayerRenderer_Render(vcSceneLayerRenderer *pSceneLayerRenderer, const udDouble4x4 &worldMatrix, const udDouble4x4 &viewProjectionMatrix, const udDouble3 &cameraPosition, const udUInt2 &screenResolution, const udFloat4 *pColourOverride = nullptr);
 
 #endif//vcSceneLayerRenderer_h__
