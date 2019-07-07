e92c98e6486d46aa2c2d940e76163b1f3bd9de3b
Paul Fox <pfox@euclideon.com>
2019-07-07 14:56:28 +1000

    Added per convert job file queues

--- src/vcConvert.h
+++ src/vcConvert.h
@@ -6,6 +6,8 @@
 #include "vcState.h"
 #include "gl/vcTexture.h"
 
+#include "udSafeDeque.h"
+
 struct vcState;
 
 enum vcConvertQueueStatus
@@ -26,6 +28,8 @@ enum vcConvertQueueStatus
 
 struct vcConvertItem
 {
+  udMutex *pMutex;
+
   vdkConvertContext *pConvertContext;
   const vdkConvertInfo *pConvertInfo;
   volatile vcConvertQueueStatus status;
@@ -35,6 +39,9 @@ struct vcConvertItem
   char comment[vcMetadataMaxLength];
   char copyright[vcMetadataMaxLength];
   char license[vcMetadataMaxLength];
+
+  const char *pItemProcessing;
+  udChunkedArray<const char*> itemsToProcess;
 
   struct
   {
@@ -49,12 +56,17 @@ struct vcConvertItem
 struct vcConvertContext
 {
   udChunkedArray<vcConvertItem*> jobs;
-  bool threadRunning;
-  udSemaphore *pSemaphore;
-  udMutex *pMutex;
-  udThread *pThread;
 
+  bool threadsRunning;
+  udRWLock *pRWLock; // Protects the `jobs` chunked array
   size_t selectedItem;
+
+  udSemaphore *pConversionSemaphore;
+  udThread *pConversionThread;
+
+  udSemaphore *pProcessingSemaphore;
+  udThread *pProcessingThread;
+
 };
 
 void vcConvert_Init(vcState *pProgramState);
@@ -62,7 +74,7 @@ void vcConvert_Deinit(vcState *pProgramState);
 
 void vcConvert_ShowUI(vcState *pProgramState);
 
-bool vcConvert_AddFile(vcState *pProgramState, const char *pFilename, bool watermark = false);
+void vcConvert_QueueFile(vcState *pProgramState, const char *pFilename);
 void vcConvert_RemoveJob(vcState *pProgramState, size_t index);
 
 #endif // !vcConvert_h__
