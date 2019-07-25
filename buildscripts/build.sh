d75b1950452a79eae100a2ae810ee8b6bf9bcd58
Paul Fox <pfox@euclideon.com>
2019-07-25 14:16:43 +1000

    Updated VDK for required changes

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -13,7 +13,7 @@ else
 fi
 
 export DEPLOYDIR="$DEV/Builds/vault/client/Pipeline_$CI_PIPELINE_ID"
-export VAULTSDK_HOME="$DEV/Builds/vault/linkedvdk/Pipeline_41602"
+export VAULTSDK_HOME="$DEV/Builds/vault/linkedvdk/Pipeline_42097"
 
 # Prepare UserGuide
 mkdir -p builds/userguide
