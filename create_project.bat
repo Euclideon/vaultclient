c1f00d56a572f0a6f40ce668baf81806b797f602
Paul Fox <pfox@euclideon.com>
2019-08-23 11:29:57 +1000

    Removed emscripten option from create menu

--- create_project.bat
+++ create_project.bat
@@ -8,15 +8,13 @@ ECHO Select the type of project you would like to create:
 IF "%forcevdk%" =="" ECHO 0. Euclideon VDK Developers (Additional Menu)
 ECHO 1. Visual Studio 2019 Solution%forcevdkText% (DirectX 11)
 ECHO 2. Visual Studio 2019 Solution%forcevdkText% (OpenGL)
-ECHO 3. Visual Studio 2019 Solution%forcevdkText% (Emscripten)
-ECHO 4. Visual Studio 2015 Solution%forcevdkText% (DirectX 11)
-ECHO 5. Visual Studio 2015 Solution%forcevdkText% (OpenGL)
+ECHO 3. Visual Studio 2015 Solution%forcevdkText% (DirectX 11)
+ECHO 4. Visual Studio 2015 Solution%forcevdkText% (OpenGL)
 
-IF "%forcevdk%" =="" CHOICE /N /C:123450 /M "[0-5]:"
-IF "%forcevdk%" NEQ "" CHOICE /N /C:12345 /M "[1-5]:"
+IF "%forcevdk%" =="" CHOICE /N /C:12340 /M "[0-4]:"
+IF "%forcevdk%" NEQ "" CHOICE /N /C:1234 /M "[1-4]:"
 
-IF ERRORLEVEL ==6 GOTO ZERO
-IF ERRORLEVEL ==5 GOTO FIVE
+IF ERRORLEVEL ==5 GOTO ZERO
 IF ERRORLEVEL ==4 GOTO FOUR
 IF ERRORLEVEL ==3 GOTO THREE
 IF ERRORLEVEL ==2 GOTO TWO
@@ -35,16 +33,11 @@ GOTO END
  GOTO END
 
 :THREE
- ECHO Creating VS2019%forcevdkText% Emscripten Project...
- 3rdParty\udcore\bin\premake-bin\premake5.exe vs2019 %forcevdk% --os=emscripten
- GOTO END
-
-:FOUR
  ECHO Creating VS2015%forcevdkText% DirectX Project...
  3rdParty\udcore\bin\premake-bin\premake5.exe vs2015 %forcevdk% --gfxapi=d3d11
  GOTO END
 
-:FIVE
+:FOUR
  ECHO Creating VS2015%forcevdkText% OpenGL Project...
  3rdParty\udcore\bin\premake-bin\premake5.exe vs2015 %forcevdk% --gfxapi=opengl
  GOTO END
