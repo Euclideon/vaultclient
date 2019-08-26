8e8a3bc71b8c8dfa0389177206c72d1645689dfd
Paul Fox <pfox@euclideon.com>
2019-08-26 14:55:11 +1000

    Added reload support for vcMedia nodes to support auto updating cameras (like traffic cameras)

--- src/scene/vcMedia.cpp
+++ src/scene/vcMedia.cpp
@@ -25,10 +25,14 @@ UDCOMPILEASSERT(udLengthOf(s_imageThumbnailSizes) == vcIS_Count, "Update Image S
 
 vcMedia::vcMedia(vdkProject *pProject, vdkProjectNode *pNode, vcState *pProgramState) :
   vcSceneItem(pProject, pNode, pProgramState),
-  m_pLoadedURI(nullptr)
-{
+  m_pLoadedURI(nullptr),
+  m_loadLoadTimeSec(0.0),
+  m_reloadTimeSecs(0.0),
+  m_pImageData(nullptr),
+  m_imageDataSize(0)
+ {
   memset(&m_image, 0, sizeof(m_image));
-  m_loadStatus = vcSLS_Loading;
+  m_loadStatus = vcSLS_Loaded;
 
   OnNodeUpdate(pProgramState);
 }
@@ -38,33 +42,58 @@ vcMedia::~vcMedia()
   vcTexture_Destroy(&m_image.pTexture);
 }
 
-void vcMedia::OnNodeUpdate(vcState *pProgramState)
+void vcMedia_LoadImage(void* pMediaPtr)
 {
-  if (m_pNode->pURI != nullptr && !udStrEqual(m_pLoadedURI, m_pNode->pURI))
+  vcMedia* pMedia = (vcMedia*)pMediaPtr;
+
+  int32_t status = udInterlockedCompareExchange(&pMedia->m_loadStatus, vcSLS_Loading, vcSLS_Pending);
+
+  void *pFileData = nullptr;
+  int64_t fileLen = 0;
+
+  if (status == vcSLS_Pending)
   {
-    udFree(m_pLoadedURI);
-    m_pLoadedURI = udStrdup(m_pNode->pURI);
+    udResult result = udFile_Load(pMedia->m_pLoadedURI, &pFileData, &fileLen);
 
-    void *pFileData = nullptr;
-    int64_t fileLen = 0;
-
-    if (m_image.pTexture != nullptr)
-      vcTexture_Destroy(&m_image.pTexture);
-
-    if (udFile_Load(m_pNode->pURI, &pFileData, &fileLen) == udR_Success)
+    if (result == udR_Success)
     {
-      if (vcTexture_CreateFromMemory(&m_image.pTexture, pFileData, fileLen))
-        m_loadStatus = vcSLS_Loaded;
-      else
-        m_loadStatus = vcSLS_Failed;
-
-      udFree(pFileData);
+      pMedia->SetImageData(&pFileData, fileLen); // Ownership is passed and pFileData should be null
+      pMedia->m_loadStatus = vcSLS_Loaded;
+    }
+    else if (result == udR_OpenFailure)
+    {
+      pMedia->m_loadStatus = vcSLS_OpenFailure;
     }
     else
     {
-      m_loadStatus = vcSLS_Failed;
+      pMedia->m_loadStatus = vcSLS_Failed;
     }
+
+    udFree(pFileData); // Should always be nullptr by this time
   }
+}
+
+void vcMedia::OnNodeUpdate(vcState *pProgramState)
+{
+  if (m_pNode->pURI == nullptr)
+  {
+    m_loadStatus = vcSLS_Failed;
+
+    if (m_image.pTexture != nullptr)
+      vcTexture_Destroy(&m_image.pTexture);
+  }
+
+  if (m_loadStatus == vcSLS_Loaded && ((m_pNode->pURI != nullptr && !udStrEqual(m_pLoadedURI, m_pNode->pURI)) || (m_reloadTimeSecs != 0.0 && m_loadLoadTimeSec + m_reloadTimeSecs < udGetEpochSecsUTCf())))
+  {
+    m_loadStatus = vcSLS_Pending;
+    udFree(m_pLoadedURI);
+    m_pLoadedURI = udStrdup(m_pNode->pURI);
+
+    udWorkerPool_AddTask(pProgramState->pWorkerPool, vcMedia_LoadImage, this, false);
+    m_loadLoadTimeSec = udGetEpochSecsUTCf();
+  }
+
+  vdkProjectNode_GetMetadataDouble(m_pNode, "reloadRate", &m_reloadTimeSecs, 0.0);
 
   m_image.ypr = udDouble3::zero();
   m_image.scale = udDouble3::one();
@@ -114,6 +143,21 @@ void vcMedia::AddToScene(vcState *pProgramState, vcRenderData *pRenderData)
     vcImageRenderInfo *pImageInfo = &m_image;
     pRenderData->images.Insert(i, &pImageInfo);
   }
+
+  if (m_reloadTimeSecs != 0.0 && m_loadLoadTimeSec + m_reloadTimeSecs < udGetEpochSecsUTCf())
+    OnNodeUpdate(pProgramState);
+
+  if (m_loadStatus == vcSLS_Loaded && m_pImageData != nullptr)
+  {
+    if (m_image.pTexture != nullptr)
+      vcTexture_Destroy(&m_image.pTexture);
+
+    if (!vcTexture_CreateFromMemory(&m_image.pTexture, m_pImageData, m_imageDataSize)) //If this doesn't load
+      m_loadStatus = vcSLS_Failed;
+
+    udFree(m_pImageData);
+    m_imageDataSize = 0;
+  }
 }
 
 void vcMedia::ApplyDelta(vcState *pProgramState, const udDouble4x4 &delta)
@@ -147,6 +191,14 @@ void vcMedia::HandleImGui(vcState *pProgramState, size_t *pItemID)
     if (ImGui::Combo(udTempStr("%s##scenePOIThumbnailSize%zu", vcString::Get("scenePOIThumbnailSize"), *pItemID), (int *)&m_image.size, imageThumbnailSizeNames, (int)udLengthOf(imageThumbnailSizeNames)))
       vdkProjectNode_SetMetadataString(m_pNode, "imagesize", s_imageThumbnailSizes[(int)m_image.size]);
   }
+
+  double reloadTimeMin = 0;
+  double reloadTimeMax = 3600; // Hourly
+  if (ImGui::SliderScalar(udTempStr("%s##scenePOILabelImageType%zu", vcString::Get("scenePOIReloadTime"), *pItemID), ImGuiDataType_Double, &m_reloadTimeSecs, &reloadTimeMin, &reloadTimeMax))
+  {
+    m_reloadTimeSecs = udClamp(m_reloadTimeSecs, reloadTimeMin, reloadTimeMax);
+    vdkProjectNode_SetMetadataDouble(m_pNode, "reloadRate", m_reloadTimeSecs);
+  }
 }
 
 void vcMedia::ChangeProjection(const udGeoZone &newZone)
@@ -157,12 +209,14 @@ void vcMedia::ChangeProjection(const udGeoZone &newZone)
   vcProject_FetchNodeGeometryAsCartesian(m_pProject, m_pNode, newZone, &pPoint, &numPoints);
   if (numPoints == 1)
     m_image.position = pPoint[0];
+
   udFree(pPoint);
 }
 
 void vcMedia::Cleanup(vcState * /*pProgramState*/)
 {
   udFree(m_pLoadedURI);
+  udFree(m_pImageData);
 }
 
 void vcMedia::SetCameraPosition(vcState *pProgramState)
@@ -173,4 +227,15 @@ void vcMedia::SetCameraPosition(vcState *pProgramState)
 udDouble4x4 vcMedia::GetWorldSpaceMatrix()
 {
   return udDouble4x4::translation(m_image.position);
+}
+
+void vcMedia::SetImageData(void **ppImageData, int64_t imageDataSize)
+{
+  // Don't do the actual loading here, this gets called from a (single) worker thread
+  if (m_pImageData == nullptr)
+  {
+    m_pImageData = *ppImageData;
+    *ppImageData = nullptr;
+    m_imageDataSize = imageDataSize;
+  }
 }
