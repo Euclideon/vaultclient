8b6d5f5f7d81e3950770c4fe070c7dee903ea9b5
Damian Madden <dmadden@euclideon.com>
2019-11-05 13:51:29 +1000

    Attempt to fix build error

--- src/imgui_ex/imgui_impl_sdl.cpp
+++ src/imgui_ex/imgui_impl_sdl.cpp
@@ -197,9 +197,8 @@ bool ImGui_ImplSDL2_ProcessEvent(const SDL_Event* event)
             return true;
         }
     case SDL_KEYDOWN:
-        {
-          down = true; // Falls through
-        }
+        down = true; 
+        // Falls through
     case SDL_KEYUP:
         {
             // Prevents backspace being grabbed by ImGui if IME composition is in progress
