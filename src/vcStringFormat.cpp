67dd270b12fb1e7da93c123a2fbdfcd4f95be82f
Damian Madden <dmadden@euclideon.com>
2020-01-24 19:04:49 +1000

    Remove duplicated code in vcStringFormat

--- src/vcStringFormat.cpp
+++ src/vcStringFormat.cpp
@@ -3,12 +3,11 @@
 #include "udPlatformUtil.h"
 #include "udStringUtil.h"
 
-const char *vcStringFormat(const char *pFormatString, const char **ppStrings, size_t numStrings)
+inline uint32_t vcStringFormat_CalculateNewLength(const char *pFormatString, const char **ppStrings, size_t numStrings, uint32_t *pOldLength)
 {
-  int len;
   int newChars = 0;
-  int currArg;
-  int i;
+  uint32_t currArg;
+  uint32_t i;
 
   for (i = 0; pFormatString[i] != '\0'; ++i)
   {
@@ -26,25 +25,27 @@ const char *vcStringFormat(const char *pFormatString, const char **ppStrings, si
 
       int length;
       currArg = udStrAtoi((pFormatString + i + 1), &length);
-      if (currArg > (int) numStrings || currArg < 0 || pFormatString[length + i + 1] != '}')
+      if (currArg > numStrings || pFormatString[length + i + 1] != '}')
         continue;
 
-      newChars += (int) udStrlen(ppStrings[currArg]);
+      newChars += (int)udStrlen(ppStrings[currArg]);
       newChars -= length + 2;
       i += length - 1;
     }
   }
 
-  len = i;
+  if (pOldLength != nullptr)
+    *pOldLength = i;
 
-  if (len == 0)
-    return pFormatString;
+  return i + newChars + 1;
+}
 
-  int newLength = len + newChars + 1;
-  char *pBuf = udAllocType(char, newLength, udAF_None);
+const char *vcStringFormat_FillBuffer(char *pBuf, const char *pFormatString, const char **ppStrings, size_t numStrings)
+{
+  uint32_t currArg;
 
-  int outPos = 0;
-  for (i = 0; pFormatString[i] != '\0'; ++i)
+  uint32_t outPos = 0;
+  for (uint32_t i = 0; pFormatString[i] != '\0'; ++i)
   {
     if (pFormatString[i] == '|' && pFormatString[i + 1] != '\0')
     {
@@ -56,7 +57,7 @@ const char *vcStringFormat(const char *pFormatString, const char **ppStrings, si
     // Format Specifier
     if (pFormatString[i] == '{')
     {
-      if (len <= i + 1 || pFormatString[i + 1] < '0' || pFormatString[i + 1] > '9')
+      if (pFormatString[i + 1] == '\0' || pFormatString[i + 1] < '0' || pFormatString[i + 1] > '9')
       {
         pBuf[outPos] = pFormatString[i];
         ++outPos;
@@ -65,14 +66,14 @@ const char *vcStringFormat(const char *pFormatString, const char **ppStrings, si
 
       int length;
       currArg = udStrAtoi((pFormatString + i + 1), &length);
-      if (currArg > (int) numStrings || currArg < 0 || pFormatString[length + i + 1] != '}')
+      if (currArg > numStrings || pFormatString[length + i + 1] != '}')
       {
         pBuf[outPos] = pFormatString[i];
         ++outPos;
         continue;
       }
 
-      int k;
+      uint32_t k;
       for (k = 0; ppStrings[currArg][k] != '\0'; ++k)
         pBuf[outPos + k] = ppStrings[currArg][k];
 
@@ -91,90 +92,28 @@ const char *vcStringFormat(const char *pFormatString, const char **ppStrings, si
   return pBuf;
 }
 
+const char *vcStringFormat(const char *pFormatString, const char **ppStrings, size_t numStrings)
+{
+  uint32_t oldLen;
+  uint32_t newLength = vcStringFormat_CalculateNewLength(pFormatString, ppStrings, numStrings, &oldLen);
+
+  if (oldLen == 0)
+    return pFormatString;
+  
+  char *pBuf = udAllocType(char, newLength, udAF_None);
+
+  return vcStringFormat_FillBuffer(pBuf, pFormatString, ppStrings, numStrings);
+}
+
 const char *vcStringFormat(char *pBuffer, size_t bufLen, const char *pFormatString, const char **ppStrings, size_t numStrings)
 {
-  int len;
-  int newChars = 0;
-  int currArg;
-  int i;
+  uint32_t oldLen;
+  uint32_t newLength = vcStringFormat_CalculateNewLength(pFormatString, ppStrings, numStrings, &oldLen);
 
-  for (i = 0; pFormatString[i] != '\0'; ++i)
-  {
-    if (pFormatString[i] == '|' && pFormatString[i + 1] != '\0')
-    {
-      ++i;
-      --newChars;
-      continue;
-    }
-    // Format specifier
-    if (pFormatString[i] == '{')
-    {
-      if (pFormatString[i + 1] == '\0' || pFormatString[i + 1] < '0' || pFormatString[i + 1] > '9')
-        continue;
-
-      int length;
-      currArg = udStrAtoi((pFormatString + i + 1), &length);
-      if (currArg > (int)numStrings || currArg < 0 || pFormatString[length + i + 1] != '}')
-        continue;
-
-      newChars += (int)udStrlen(ppStrings[currArg]);
-      newChars -= length + 2;
-      i += length - 1;
-    }
-  }
-
-  len = i;
-  int newLength = len + newChars + 1;
-
-  if ((int)bufLen < newLength || len == 0)
+  if ((uint32_t)bufLen < newLength || oldLen == 0)
     return pFormatString;
 
-  int outPos = 0;
-  for (i = 0; pFormatString[i] != '\0'; ++i)
-  {
-    if (pFormatString[i] == '|' && pFormatString[i + 1] != '\0')
-    {
-      ++i;
-      pBuffer[outPos] = pFormatString[i];
-      ++outPos;
-      continue;
-    }
-    // Format Specifier
-    if (pFormatString[i] == '{')
-    {
-      if (pFormatString[i + 1] == '\0' || pFormatString[i + 1] < '0' || pFormatString[i + 1] > '9')
-      {
-        pBuffer[outPos] = pFormatString[i];
-        ++outPos;
-        continue;
-      }
-
-      int length;
-      currArg = udStrAtoi((pFormatString + i + 1), &length);
-      if (currArg > (int)numStrings || currArg < 0 || pFormatString[length + i + 1] != '}')
-      {
-        pBuffer[outPos] = pFormatString[i];
-        ++outPos;
-        continue;
-      }
-
-      int k;
-      for (k = 0; ppStrings[currArg][k] != '\0'; ++k)
-        pBuffer[outPos + k] = ppStrings[currArg][k];
-
-      i += length + 1;
-      outPos += k - 1;
-    }
-    else
-    {
-      pBuffer[outPos] = pFormatString[i];
-    }
-
-    ++outPos;
-  }
-  pBuffer[outPos] = '\0';
-
-  return pBuffer;
+  return vcStringFormat_FillBuffer(pBuffer, pFormatString, ppStrings, numStrings);
 }
 
 const char *vcStringFormat(const char *pFormatString, const char *pString)
