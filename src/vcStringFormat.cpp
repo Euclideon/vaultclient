437c53139ef70656d5afd279f0e307b81625e534
Damian Madden <dmadden@euclideon.com>
2020-01-29 11:10:39 +1000

    Change non-buffer-argument version of vcStringFormat to call buffer-argument version

--- src/vcStringFormat.cpp
+++ src/vcStringFormat.cpp
@@ -50,53 +50,7 @@ const char *vcStringFormat(const char *pFormatString, const char **ppStrings, si
   
   char *pBuf = udAllocType(char, newLength, udAF_None);
 
-  uint32_t currArg;
-  uint32_t outPos = 0;
-  for (uint32_t i = 0; pFormatString[i] != '\0'; ++i)
-  {
-    if (pFormatString[i] == '|' && pFormatString[i + 1] != '\0')
-    {
-      ++i;
-      pBuf[outPos] = pFormatString[i];
-      ++outPos;
-      continue;
-    }
-    // Format Specifier
-    if (pFormatString[i] == '{')
-    {
-      if (pFormatString[i + 1] < '0' || pFormatString[i + 1] > '9')
-      {
-        pBuf[outPos] = pFormatString[i];
-        ++outPos;
-        continue;
-      }
-
-      int length;
-      currArg = udStrAtou((pFormatString + i + 1), &length);
-      if (currArg > numStrings || pFormatString[length + i + 1] != '}')
-      {
-        pBuf[outPos] = pFormatString[i];
-        ++outPos;
-        continue;
-      }
-
-      uint32_t k;
-      for (k = 0; ppStrings[currArg][k] != '\0'; ++k)
-        pBuf[outPos + k] = ppStrings[currArg][k];
-
-      i += length + 1;
-      outPos += k - 1;
-    }
-    else
-    {
-      pBuf[outPos] = pFormatString[i];
-    }
-
-    ++outPos;
-  }
-  pBuf[outPos] = '\0';
-
-  return pBuf;
+  return vcStringFormat(pBuf, newLength, pFormatString, ppStrings, numStrings);
 }
 
 const char *vcStringFormat(char *pBuf, size_t bufLen, const char *pFormatString, const char **ppStrings, size_t numStrings, size_t *pTotalWritten)
