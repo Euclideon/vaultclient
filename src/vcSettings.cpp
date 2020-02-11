a5bddee30c3abddce36a2e838f635351d0b223be
Paul Fox <pfox@euclideon.com>
2020-02-11 15:25:40 +1000

    Removed saving and loading of docks

--- src/vcSettings.cpp
+++ src/vcSettings.cpp
@@ -100,7 +100,7 @@ bool vcSettings_Load(vcSettings *pSettings, bool forceReset /*= false*/, vcSetti
   if (!pSettings->noLocalStorage && pSettings->pSaveFilePath == nullptr)
     vcSettings_InitializePrefPath(pSettings);
 
-  if (!forceReset && pSettings->docksLoaded != vcSettings::vcDockLoaded::vcDL_ForceReset && pSettings->pSaveFilePath != nullptr)
+  if (!forceReset && pSettings->pSaveFilePath != nullptr)
   {
     char buffer[vcMaxPathLength];
     udSprintf(buffer, "%ssettings.json", pSettings->pSaveFilePath);
@@ -329,89 +329,6 @@ bool vcSettings_Load(vcSettings *pSettings, bool forceReset /*= false*/, vcSetti
     pSettings->camera.lockAltitude = (data.Get("camera.moveMode").AsInt(0) == 1);
   }
 
-  if (forceReset && (group == vcSC_Docks || group == vcSC_All))
-  {
-    pSettings->docksLoaded = vcSettings::vcDockLoaded::vcDL_ForceReset;
-  }
-  else if (group == vcSC_Docks)
-  {
-    if (data.Get("dock").IsArray())
-    {
-      pSettings->docksLoaded = vcSettings::vcDockLoaded::vcDL_True;
-
-      pSettings->window.windowsOpen[vcDocks_Scene] = data.Get("frames.scene").AsBool(true);
-      pSettings->window.windowsOpen[vcDocks_SceneExplorer] = data.Get("frames.explorer").AsBool(true);
-      pSettings->window.windowsOpen[vcDocks_Convert] = data.Get("frames.convert").AsBool(false);
-
-      udJSONArray *pDocks = data.Get("dock").AsArray();
-      size_t numNodes = pDocks->length;
-
-      ImGui::DockContextClearNodes(GImGui, 0, true);
-
-      ImGuiDockNodeSettings *pDockNodes = udAllocType(ImGuiDockNodeSettings, numNodes, udAF_Zero);
-
-      for (size_t i = 0; i < pDocks->length; ++i)
-      {
-        udJSON *pDock = pDocks->GetElement(i);
-
-        pDockNodes[i].ID = pDock->Get("id").AsInt();
-        pDockNodes[i].Pos = ImVec2ih((short)pDock->Get("x").AsInt(), (short)pDock->Get("y").AsInt());
-        pDockNodes[i].Size = ImVec2ih((short)pDock->Get("w").AsInt(), (short)pDock->Get("h").AsInt());
-        pDockNodes[i].SizeRef = ImVec2ih((short)pDock->Get("wr").AsInt(), (short)pDock->Get("hr").AsInt());
-
-        if (pDock->Get("parent").IsNumeric())
-          pDockNodes[i].ParentID = pDock->Get("parent").AsInt();
-
-        if (pDock->Get("split").IsString())
-          pDockNodes[i].SplitAxis = (*(pDock->Get("split").AsString()) == 'X') ? (signed char)ImGuiAxis_X : (signed char)ImGuiAxis_Y;
-        else
-          pDockNodes[i].SplitAxis = ImGuiAxis_None;
-
-        pDockNodes[i].IsCentralNode = (char)pDock->Get("central").AsInt();
-        pDockNodes[i].IsDockSpace = (char)pDock->Get("dockspace").AsInt();
-        pDockNodes[i].IsHiddenTabBar = (char)pDock->Get("hiddentabbar").AsInt();
-        pDockNodes[i].Depth = (char)pDock->Get("depth").AsInt();
-
-        if (pDock->Get("windows").IsArray())
-        {
-          udJSONArray *pWindows = pDock->Get("windows").AsArray();
-
-          for (size_t j = 0; j < pWindows->length; ++j)
-          {
-            udJSON *pJSONWindow = pWindows->GetElement(j);
-
-            ImGuiWindow* pWindow = ImGui::FindWindowByName(pJSONWindow->Get("name").AsString());
-            if (pWindow)
-            {
-              pWindow->DockId = pDockNodes[i].ID;
-              pWindow->DockOrder = (short)pJSONWindow->Get("index").AsInt();
-              pWindow->Collapsed = pJSONWindow->Get("collapsed").AsBool();
-              if (pJSONWindow->Get("visible").AsBool())
-              {
-                for (size_t k = 0; k < sizeof(pSettings->pActive); ++k)
-                {
-                  if (pSettings->pActive[k] == nullptr)
-                  {
-                    pSettings->pActive[k] = pWindow;
-                    break;
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-
-      ImGui::DockContextBuildNodesFromSettings(GImGui, pDockNodes, (int)numNodes);
-      udFree(pDockNodes);
-    }
-    else
-    {
-      vcSettings_Load(pSettings, true, vcSC_Docks);
-      goto epilogue;
-    }
-  }
-
   if (group == vcSC_Languages || group == vcSC_All)
   {
     const char *pFileContents = nullptr;
@@ -440,59 +357,8 @@ bool vcSettings_Load(vcSettings *pSettings, bool forceReset /*= false*/, vcSetti
     udFree(pFileContents);
   }
 
-epilogue:
   udFree(pSavedData);
   return true;
-}
-
-void vcSettings_RecurseDocks(ImGuiDockNode *pNode, udJSON &out, int *pDepth)
-{
-  udJSON data;
-  data.Set("id = %d", (int)pNode->ID);
-  if (pNode->ParentNode)
-    data.Set("parent = %d", (int)pNode->ParentNode->ID);
-
-  data.Set("central = %d", (int)pNode->IsCentralNode());
-  data.Set("dockspace = %d", (int)pNode->IsDockSpace());
-  data.Set("hiddentabbar = %d", (int)pNode->IsHiddenTabBar());
-  data.Set("depth = %d", *pDepth);
-
-  for (int i = 0; i < pNode->Windows.size(); ++i)
-  {
-    udJSON window;
-
-    // Only want the part '###xxxxDock' so any language works
-    const char *pIDName = udStrchr(pNode->Windows[i]->Name, "#");
-
-    window.Set("name = '%s'", pIDName);
-    window.Set("index = %d", i);
-    window.Set("collapsed = %d", (int)pNode->Windows[i]->Collapsed);
-    window.Set("visible = %d", (int)pNode->Windows[i]->DockTabIsVisible);
-
-    data.Set(&window, "windows[]");
-  }
-
-  data.Set("x = %f", pNode->Pos.x);
-  data.Set("y = %f", pNode->Pos.y);
-  data.Set("w = %f", pNode->Size.x);
-  data.Set("h = %f", pNode->Size.y);
-  data.Set("wr = %f", pNode->SizeRef.x);
-  data.Set("hr = %f", pNode->SizeRef.y);
-
-  if (pNode->IsSplitNode())
-    data.Set("split = '%s'", (pNode->SplitAxis == 1) ? "Y" : "X");
-
-  out.Set(&data, "dock[]");
-  ++(*pDepth);
-
-  for (size_t i = 0; i < udLengthOf(pNode->ChildNodes); ++i)
-  {
-    if (pNode->ChildNodes[i] != nullptr)
-    {
-      vcSettings_RecurseDocks(pNode->ChildNodes[i], out, pDepth);
-      --(*pDepth);
-    }
-  }
 }
 
 #if UDPLATFORM_EMSCRIPTEN
@@ -676,11 +542,6 @@ bool vcSettings_Save(vcSettings *pSettings)
 
   for (size_t i = 0; i < vcB_Count; ++i)
     data.Set("keys.%s = %d", vcHotkey::GetBindName((vcBind)i), vcHotkey::Get((vcBind)i));
-
-  int depth = 0;
-  ImGuiDockNode *pRootNode = ImGui::DockBuilderGetNode(pSettings->rootDock);
-  if (pRootNode != nullptr && !pRootNode->IsEmpty())
-    vcSettings_RecurseDocks(ImGui::DockNodeGetRootNode(pRootNode), data, &depth);
 
   // Save
   const char *pSettingsStr;
