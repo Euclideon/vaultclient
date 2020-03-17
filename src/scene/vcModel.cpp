c4a10c07d48b791cb8b0dc6d7be9b67947d69271
Asuna Wu <awu@euclideon.com>
2020-03-17 13:33:06 +1000

    Removed matrix equivalent check.

--- src/scene/vcModel.cpp
+++ src/scene/vcModel.cpp
@@ -488,7 +488,7 @@ void vcModel::HandleContextMenu(vcState *pProgramState)
   ImGui::Separator();
 
 #if VC_HASCONVERT
-  if (((m_pPreferredProjection == nullptr && pProgramState->gis.SRID == 0) || (m_pPreferredProjection != nullptr && m_pPreferredProjection->srid == pProgramState->gis.SRID)) && (m_defaultMatrix == m_sceneMatrix))
+  if ((m_pPreferredProjection == nullptr && pProgramState->gis.SRID == 0) || (m_pPreferredProjection != nullptr && m_pPreferredProjection->srid == pProgramState->gis.SRID))
   {
     if (ImGui::BeginMenu(vcString::Get("sceneExplorerExportPointCloud")))
     {
