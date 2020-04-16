16b83b93a551b3e7fa8fdbf42c5ed95e3635e093
Paul Fox <pfox@euclideon.com>
2020-04-17 01:00:42 +1000

    Updated to use latest VDK

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,7 @@
 # include <windows.h>
 #endif
 
-UDCOMPILEASSERT(VDK_MAJOR_VERSION == 0 && VDK_MINOR_VERSION == 5 && VDK_PATCH_VERSION == 2, "This version of VDK is not compatible");
+UDCOMPILEASSERT(VDK_MAJOR_VERSION == 0 && VDK_MINOR_VERSION == 6 && VDK_PATCH_VERSION == 0, "This version of VDK is not compatible");
 
 #if UDPLATFORM_WINDOWS && !defined(NDEBUG)
 #  include <crtdbg.h>
