d646cec7eccfb331a9dbc4f9e563118a176cc783
Joshua Sosnowski <jsosnowski@euclideon.com>
2020-05-13 16:20:59 +1000

    PR Fixes 2

--- src/vcSettingsUI.cpp
+++ src/vcSettingsUI.cpp
@@ -942,16 +942,25 @@ void vcSettingsUI_BasicMapSettings(vcState *pProgramState)
   }
 
   if (ImGui::Button(vcString::Get("settingsMapECEFMode")))
-  {    
+  {
+    int32_t newSRID = -1;
+
     if (pProgramState->geozone.srid == vcPSZ_WGS84ECEF)
     {
-      if (pProgramState->previousSRID != -1)
-        pProgramState->geozone.srid = pProgramState->previousSRID;
+      newSRID = pProgramState->previousSRID;
     }
     else
     {
       pProgramState->previousSRID = pProgramState->geozone.srid;
-      pProgramState->geozone.srid = vcPSZ_WGS84ECEF;
+      newSRID = vcPSZ_WGS84ECEF;
+    }
+
+    if (newSRID != -1)
+    {
+      udGeoZone zone = {};
+      udGeoZone_SetFromSRID(&zone, newSRID);
+      vcGIS_ChangeSpace(&pProgramState->geozone, zone, &pProgramState->camera.position);
+      pProgramState->activeProject.pFolder->ChangeProjection(zone);
     }
   }
 }
