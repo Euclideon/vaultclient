4a486d31f03ca31c335040979b49fc1638a8e4e1
Frank Hart <fhart@euclideon.com>
2020-06-30 16:09:57 +1000

    Removed 'override' keyword to prevent errors

--- src/scene/vcModel.h
+++ src/scene/vcModel.h
@@ -44,7 +44,7 @@ public:
   void ApplyDelta(vcState *pProgramState, const udDouble4x4 &delta);
 
   void HandleSceneExplorerUI(vcState *pProgramState, size_t *pItemID);
-  virtual void HandleSceneEmbeddedUI(vcState *pProgramState) override;
+  void HandleSceneEmbeddedUI(vcState *pProgramState);
   void HandleContextMenu(vcState *pProgramState);
 
   void Cleanup(vcState *pProgramState);
