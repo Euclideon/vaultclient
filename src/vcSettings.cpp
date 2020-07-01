4c2497e190b1680b48167ce9fb761c47fbbe6d00
Peter Adams <peter.adams1014@gmail.com>
2020-07-01 16:17:44 +1000

    Minor bug fixes
    Fixes AB#1814
    Fixes AB#1808

--- src/vcSettings.cpp
+++ src/vcSettings.cpp
@@ -6,6 +6,7 @@
 #include "udStringUtil.h"
 
 #include "imgui.h"
+#include "imgui_ex/vcImGuiSimpleWidgets.h"
 
 #include "vcClassificationColours.h"
 #include "vcStringFormat.h"
@@ -1023,10 +1024,10 @@ void vcSettings_LoadBranding(vcState *pState)
   udStrcpy(pState->branding.copyrightName, branding.Get("copyright").AsString("Euclideon Pty Ltd"));
   udStrcpy(pState->branding.supportEmail, branding.Get("supportEmail").AsString("support@euclideon.com"));
 
-  const char* DefaultColours[] = { "B5A245", "E3D9A8", "CDBC71", "998523" };
+  const char* DefaultColours[] = { "45A2B5", "A8D9E3", "71BCCD", "238599" };
 
   for (size_t i = 0; i < udMin(udLengthOf(pState->branding.colours), udLengthOf(DefaultColours)); ++i)
-    pState->branding.colours[i] = 0xFF000000 | udStrAtou(branding.Get("logincolours[%zu]", i).AsString(DefaultColours[i]), nullptr, 16);
-    
+    pState->branding.colours[i] = vcIGSW_BGRAToRGBAUInt32(0xFF000000 | udStrAtou(branding.Get("logincolours[%zu]", i).AsString(DefaultColours[i]), nullptr, 16));
+
   udFree(pBrandStrings);
 }
