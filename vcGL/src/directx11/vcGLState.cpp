e6328d43b0219d0b3dbeabcc30ccd51c948adaf0
Peter Adams <padams@euclideon.com>
2020-09-01 14:04:00 +1000

    Potentially fixed d3d blending issue on some GPUs.
    Fixes AB#2001

--- vcGL/src/directx11/vcGLState.cpp
+++ vcGL/src/directx11/vcGLState.cpp
@@ -201,8 +201,8 @@ bool vcGLState_SetBlendMode(vcGLStateBlendMode blendMode, bool force /*= false*/
     desc.AlphaToCoverageEnable = false;
     desc.RenderTarget[0].BlendEnable = true;
     desc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
-    desc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
-    desc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ONE;
+    desc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_SRC_ALPHA;
+    desc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_DEST_ALPHA;
     desc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
     desc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
 
