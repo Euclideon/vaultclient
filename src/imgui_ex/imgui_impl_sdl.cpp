0bfc94dab2eb5375ac7bd2e64b510c7e68817c6d
Samuel Surtees <ssurtees@euclideon.com>
2020-09-09 19:06:23 +1000

    Single viewport platforms no longer require focus to update mouse position

--- src/imgui_ex/imgui_impl_sdl.cpp
+++ src/imgui_ex/imgui_impl_sdl.cpp
@@ -433,8 +433,7 @@ static void ImGui_ImplSDL2_UpdateMousePosAndButtons()
     SDL_CaptureMouse(any_mouse_button_down ? SDL_TRUE : SDL_FALSE);
 #else
     // SDL 2.0.3 and before: single-viewport only
-    if (SDL_GetWindowFlags(g_Window) & SDL_WINDOW_INPUT_FOCUS)
-        io.MousePos = ImVec2(mouse_x_local / g_Scaling, mouse_y_local / g_Scaling);
+    io.MousePos = ImVec2(mouse_x_local / g_Scaling, mouse_y_local / g_Scaling);
 #endif
 }
 
