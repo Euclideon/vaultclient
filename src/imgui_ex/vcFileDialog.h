0781e5292d32fa1c1d7a12bddbb9ff6963c9c744
Asuna Wu <asunawu1@gmail.com>
2020-09-16 19:39:13 +1000

    Fix build error.

--- src/imgui_ex/vcFileDialog.h
+++ src/imgui_ex/vcFileDialog.h
@@ -63,6 +63,7 @@ template <size_t N, size_t M> inline void vcFileDialog_Open(vcState *pProgramSta
   udUnused(SupportedTileTypes_ConvertExport);
   udUnused(SupportedTileTypes_QueryExport);
   udUnused(SupportedFileTypes_ConvertImport);
+  udUnused(SupportedFileTypes_ShapeFile);
 }
 
 void vcFileDialog_ShowModal(vcState *pProgramState);
